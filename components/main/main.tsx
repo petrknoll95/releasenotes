b036d8f6dea1d37f99412c6df96e13649abc82f6
Petr Knoll <hello@petrknoll.co>
2025-05-20 02:13:35 +0200

    Main layout

--- components/main/main.tsx
+++ components/main/main.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function Main() {
     return (
-        <div className="flex-grow w-full lg:w-3/5 md:min-h-screen"></div>
+        <div className="flex-grow w-full lg:w-3/5 md:min-h-screen flex flex-col items-stretch">
+            <div className="p-6 border-b border-[var(--border-secondary)]"></div>
+            <div className="p-6 border-b border-[var(--border-secondary)]">
+                <div className="w-full aspect-[16/9]"></div>
+            </div>
+            <div className="p-6"></div>
+        </div>
     )
 }
